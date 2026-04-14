73e6894d161c4853e17970024a752e427ec1bc1c
Documenter.jl <documenter@juliadocs.github.io>
2026-04-14 15:29:21 +0000

    build based on 6c05dd7

---
+++ dev/examples/Kazmierczak2024.jl
@@ -0,0 +1,15 @@
+using Hydrology
+
+float_type = Float64
+path = "/Users/taange001/Documents/Coding/Hydrology.jl/src/input/Kazmierczak2024/THWAITES2km_m3_HAB_toto.mat"
+data_loading_function = load_Kazmierczak2024
+c = PhysicalConstants{float_type}()
+HM = HydrologyModel(path, data_loading_function, c)
+
+update_q!(HM)
+update_N!(HM)
+
+visualize_field(HM.fields, :q)
+visualize_field(HM.fields, :N)
+
+println("Finished.")
