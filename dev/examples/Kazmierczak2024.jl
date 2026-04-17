4f1fd5d5f56bd38fb084e22393129b20793cc282
Documenter.jl <documenter@juliadocs.github.io>
2026-04-17 12:12:18 +0000

    build based on 78e6c44

--- dev/examples/Kazmierczak2024.jl
+++ dev/examples/Kazmierczak2024.jl
@@ -1,15 +1,33 @@
 using Hydrology
 
-float_type = Float64
-path = "/Users/taange001/Documents/Coding/Hydrology.jl/src/input/Kazmierczak2024/THWAITES2km_m3_HAB_toto.mat"
-data_loading_function = load_Kazmierczak2024
-c = PhysicalConstants{float_type}()
-HM = HydrologyModel(path, data_loading_function, c)
+# Type of the physical fields
+T = Float64 
 
-update_q!(HM)
-update_N!(HM)
+# Path to data; run the file from the repository Hydrology.jl path
+path = "$(@__DIR__)/input/Kazmierczak2024/THWAITES2km_m3_HAB_toto.mat"
 
-visualize_field(HM.fields, :q)
-visualize_field(HM.fields, :N)
+# Function to load the data
+data_loading_function = load_Kazmierczak
+
+# Load the data as they are saved from the repository of Kazmierczak et al 2024 for Fig. S3
+Nx, Ny, xlims, ylims, mask, h, b, abs_v_b, A_visc, ṁ_over_ρ_w, κ = load_Kazmierczak(path)
+
+# Build the grid as an Oceananigans rectilinear grid
+grid = OGRectHydroGrid(Nx, Ny, xlims, ylims; T = T)
+
+# Build the model of Kazmierczak et al 2024
+model = KazmierczakHydroModel(grid, κ, abs_v_b, A_visc)
+
+# Build the hydrology state that is common to all hydrology models
+state = HydroState(grid, mask, h, b, ṁ_over_ρ_w*model.ρ_w)
+
+# Create a Simulation struct for Kazmierczak 2024 et al which is a steady state calculation
+sim = SteadyStateSimulation(model, grid, state)
+
+# Run the simulation
+run!(sim)
+
+visualize_field(model.q, state.mask; plot_title = "q")
+visualize_field(state.N, state.mask; plot_title = "N")
 
 println("Finished.")
