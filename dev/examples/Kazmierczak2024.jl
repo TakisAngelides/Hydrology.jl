71c81c48ae3be497885db26d6830cbbfa396a9c8
Documenter.jl <documenter@juliadocs.github.io>
2026-04-24 10:12:30 +0000

    build based on 7697ef2

--- dev/examples/Kazmierczak2024.jl
+++ dev/examples/Kazmierczak2024.jl
@@ -2,14 +2,17 @@
 # [Kazmierczak et al 2024](@id Kazmierczak2024)
 This is an example of how to run the FastHydrology.jl package for the steady state problem of Kazmierczak et al 2024.
 =#
+
 using FastHydrology
+using Statistics
+using FileIO
+using CairoMakie
 
 # How to load data from a file to get the necessary inputs to initialize the grid, model and state for the Kazmierczak et al 2024 paper.
 
 T = Float64 # Type of the physical fields
 path = "$(@__DIR__)/input/Kazmierczak2024/THWAITES2km_m3_HAB_toto.mat" # Path to data for Fig. S3 of Kazmierczak et al 2024 which focuses on Thwaites glacier.
-data_loading_function = load_Kazmierczak # Function to load the data.
-Nx, Ny, xlims, ylims, mask, h, b, abs_v_b, A_visc, ṁ_over_ρ_w, κ = load_Kazmierczak(path)
+Nx, Ny, xlims, ylims, mask, h, b, abs_v_b, A_visc, ṁ_over_ρ_w, κ = load_Kazmierczak(path; bed_rheology = :hard)
 
 # Here we prepare a grid using the Oceananigans rectilinear grid.
 
@@ -35,11 +38,59 @@ sim = SteadyStateSimulation(model, grid, state);
 
 run!(sim)
 
-# Now we can visualize the resulting water flux q [m² s⁻¹].
-fig_q = visualize_field(model.q, state.mask; plot_title = "q")
+# Applying some rescaling for plotting.
+
+model.q .*= perSecond2perYear(1.0) / 1e4 # makes q [10⁴ m² a⁻¹]
+state.N .*= 1e-6 # makes N [MPa]
+model.q .= mask_field(model.q, state.mask, NaN)
+state.N .= mask_field(state.N, state.mask, NaN)
+state.W .= mask_field(state.W, state.mask, NaN)
+display_flag = false
+
+# Now we can visualize the resulting water flux q [10⁴ m² a⁻¹].
+
+fig_q = visualize_field(model.q; plot_title = "Distributed water flux q [10⁴ m² a⁻¹]", transpose_data = true, display_flag = display_flag, colorrange = (0, 10))
 
 # The effective pressure N [MPa].
-fig_N = visualize_field(state.N, state.mask; plot_title = "N")
 
-# The water layer thickness W [m]
-fig_W = visualize_field(state.W, state.mask; plot_title = "W")
+fig_N = visualize_field(state.N; plot_title = "Effective pressure N [MPa]", transpose_data = true, display_flag = display_flag, colorrange = (0, 10))
+
+# The water layer thickness W [m].
+
+fig_W = visualize_field(state.W; plot_title = "Water thickness W [m]", transpose_data = true, display_flag = display_flag, colorrange = extrema(filter(!isnan, state.W.data)))
+
+# Using a similar setup to the one above but with model.longcoupwater = 0.0, we can plot results for the whole of Antarctica.
+
+function plot(f, r)
+    img = load("$(@__DIR__)/figures/Kaz24_yelmox_$(f)_$(r)km.png")
+    fig = CairoMakie.Figure()
+    ax = Axis(fig[1, 1], yreversed = true)
+    image!(ax, img')
+    ax.aspect = DataAspect()
+    hidedecorations!(ax)
+    return fig    
+end
+
+# Water flux [m²/s] 32km resolution.
+
+fig = plot("q", "32")
+
+# Water flux [m²/s] 16km resolution.
+
+fig = plot("q", "16")
+
+# Water thickness [m] 32km resolution.
+
+fig = plot("W", "32")
+
+# Water thickness [m] 16km resolution.
+
+fig = plot("W", "16")
+
+# Effective pressure [Pa] 32km resolution.
+
+fig = plot("N", "32")
+
+# Effective pressure [Pa] 16km resolution.
+
+fig = plot("N", "16")
